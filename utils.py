92068060d67abdaba9abf6cf8063b19b89668aae
Jay Harris <jay.harris@outlook.co.nz>
2019-03-12 15:27:03 +1100

    Cleans up run

--- utils.py
+++ utils.py
@@ -1,14 +1,31 @@
 import os
+import json
+
 
 def get_executables_lazy():
-	for name in os.listdir('out/Default'):
-		path = f'out/Default/{name}'
-		if not os.path.isfile(path): continue
-		if not os.access(path, os.X_OK): continue
+    if not os.path.exists('out/Default'): return ['chrome']
 
-		yield name
+    for name in os.listdir('out/Default'):
+        path = f'out/Default/{name}'
+        if not os.path.isfile(path):
+            continue
+        if not os.access(path, os.X_OK): continue
+
+        yield name
+
+
 def get_executables():
     return list(get_executables_lazy())
 
+
 def get_configurations():
-	return os.listdir('out/')
+    if not os.path.exists('out/'):
+        return ['Default']
+
+    return os.listdir('out/')
+
+
+def load_config():
+    with open('config.json') as f:
+        json_string = f.read()
+        return json.loads(json_string)
